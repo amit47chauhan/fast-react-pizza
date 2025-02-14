d6c2398c283fc723718609a9b8adb879c1c36f69
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-02-14 16:38:05 +0530

    fetching orders from api

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Error from "./ui/Error";
 import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/menu/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, {loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/appLayout";
 
 const router = createBrowserRouter([
@@ -32,8 +32,10 @@ const router = createBrowserRouter([
         element: <CreateOrder />,
       },
       {
-        path: "/order:orderId",
+        path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
