cfa352e15d5736003a25b7fcf2848a194ee7f90f
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-02-26 12:20:01 +0530

    Creating userReducer in userSlice and storing into the store.js and introducing the provider in main.jsx to provide global state to the app

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,7 @@
-
 export default function Loader() {
   return (
-    <div className="loader">
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center ">
+      <div className="loader"></div>
     </div>
-  )
+  );
 }
